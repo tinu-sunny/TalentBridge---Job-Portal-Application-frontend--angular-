5c861bc5e4ea97282cfeae35603c62399f0c850b
TS <tinusunny04@gmail.com>
2026-02-05 19:53:13 +0530

    header desing complted

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
