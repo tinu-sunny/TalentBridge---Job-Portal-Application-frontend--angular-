7febe1b7e90ff32cacd93a99d68928053da6eba2
TS <tinusunny04@gmail.com>
2026-02-05 23:18:16 +0530

    users desing complted

--- src/app/users/pages/jobview/jobview.ts
+++ src/app/users/pages/jobview/jobview.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-jobview',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './jobview.html',
   styleUrl: './jobview.css',
 })
--- src/app/users/pages/userlandingpage/userlandingpage.ts
+++ src/app/users/pages/userlandingpage/userlandingpage.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-userlandingpage',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './userlandingpage.html',
   styleUrl: './userlandingpage.css',
 })
