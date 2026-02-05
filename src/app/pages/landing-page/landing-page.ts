357fbdd341e375db8775a1de5149b8dd0dd94d43
TS <tinusunny04@gmail.com>
2026-02-05 20:03:57 +0530

    landing page and footer desing complted

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
 import { Footer } from "../../components/footer/footer";
+import { Auth } from "../../components/auth/auth";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Auth],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
