0bc9ad50b9927faec6233775a55314ed772a34e2
TS <tinusunny04@gmail.com>
2026-02-05 19:35:14 +0530

    created the modules and folder struture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './pages/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:LandingPage
+    }
+];
